13d47f4fa2ea96a9734c87d66f88954cc04eb6a5
sreyeg <santiagoreyes.g@gmail.com>
2025-01-20 13:39:56 -0800

    resolve clue 2 - 6

--- clues.sql
+++ clues.sql
@@ -3,7 +3,8 @@
 --and we'll start looking for her there.
  
 -- Write SQL query here
-SELECT min(population) FROM countries WHERE region = 'Southern Europe' AND population = 1000;
+\c world
+SELECT name FROM countries WHERE region = 'Southern Europe' AND population = 1000;
 -- Clue #2: Now that we're here, we have insight that 
 -- Carmen was seen attending language classes in this country's officially recognized language. 
 -- Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
@@ -28,21 +29,40 @@ WHERE region = 'Southern Europe' AND language = 'Italian';
 -- We're following our gut on this one; find out what other city in that country she might be flying to.
 
 -- Write SQL query here
+SELECT countries.code, countries.name, countries.region, countrylanguages.language
+FROM countries
+JOIN countrylanguages 
+ON countries.code = countrylanguages.countrycode
+JOIN cities
+on countries.code = cities.countrycode
+WHERE region = 'Southern Europe' AND language = 'Italian' AND countries.name = cities.name;
 
-
--- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
+-- Clue #5: Oh no, she pulled a switch – 
+-- there are two cities with very similar names, but in totally different parts of the globe! 
+-- She's headed to South America as we speak; go find a city whose name is like the one we were headed to, 
+-- but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 -- Write SQL query here
-
+-- City and code query
+SELECT countries.code,countries.name, countries.region, cities.name
+FROM countries
+JOIN cities
+ON countries.code = cities.countrycode
+WHERE region = 'South America' and cities.name LIKE '%San Mar%';
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
 
 -- Write SQL query here
-
-
--- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
+SELECT district
+FROM cities
+WHERE name LIKE 'General San Mart%';
+-- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, 
+-- and she beat us to the boarding gates. We have one chance to catch her, 
+-- we just have to know where she's heading and beat her to the landing dock. 
+-- Lucky for us, she's getting cocky. She left us a note (below), 
+-- and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
 
 
 --               Our playdate of late has been unusually fun –
@@ -53,4 +73,8 @@ WHERE region = 'Southern Europe' AND language = 'Italian';
 --               In a city of ninety-one thousand and now, eighty five.
 
 
--- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+-- We're counting on you, gumshoe. Find out where she's headed, send us the info, 
+-- and we'll be sure to meet her at the gates with bells on.
+SELECT * 
+FROM cities
+WHERE population = 91084;
